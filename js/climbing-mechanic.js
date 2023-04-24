0d5b5fd6846e7fc2a98efb001b8422fb48c66a53
NSTCG <nithinsteven32@gmail.com>
2023-04-24 17:14:36 +0200

    migrate Climbing Game to 1.0.0

--- js/climbing-mechanic.js
+++ js/climbing-mechanic.js
@@ -1,27 +1,35 @@
-WL.registerComponent('climbing-mechanic', {
-    handedness: {type: WL.Type.Enum, values: ['left', 'right'], default: 'left'},
-    player: {type: WL.Type.Object},
-}, {
-    init: function() {
+import {Component, Type} from '@wonderlandengine/api';
+import {vec3} from 'gl-matrix';
+
+export class ClimbingMechanic extends Component {
+    static TypeName = 'climbing-mechanic';
+    static Properties = {
+        handedness: {type: Type.Enum, values: ['left', 'right'], default: 'left'},
+        player: {type: Type.Object},
+    };
+
+    init() {
         this.overlappedGrip = null;
         this.heldGrip = null;
 
         this.grippedPosition = new Float32Array(3);
         this.newPosition = new Float32Array(3);
         this.diff = new Float32Array(3);
-    },
-    start: function() {
+    }
+
+    start() {
         this.collision = this.object.getComponent('collision');
 
-        WL.onXRSessionStart.push(this.onXRSessionStart.bind(this));
+        this.engine.onXRSessionStart.push(this.onXRSessionStart.bind(this));
 
         this.handedness = ['left', 'right'][this.handedness];
-    },
-    update: function(dt) {
+    }
+
+    update(dt) {
         const overlaps = this.collision.queryOverlaps();
-        if(overlaps.length > 0) {
+        if (overlaps.length > 0) {
             const comp = overlaps[0];
-            if(comp.object.name.endsWith('Controller')) {
+            if (comp.object.name.endsWith('Controller')) {
                 return;
             }
             this.overlappedGrip = comp;
@@ -29,27 +37,28 @@ WL.registerComponent('climbing-mechanic', {
             this.overlappedGrip = null;
         }
 
-        if(this.heldGrip != null) {
+        if (this.heldGrip != null) {
             this.object.getTranslationWorld(this.newPosition);
-            glMatrix.vec3.sub(this.diff, this.grippedPosition, this.newPosition);
+            vec3.sub(this.diff, this.grippedPosition, this.newPosition);
             this.player.translate(this.diff);
         }
-    },
+    }
 
-    onXRSessionStart: function(session) {
+    onXRSessionStart(session) {
         session.addEventListener('selectstart', this.onTriggerPressed.bind(this));
         session.addEventListener('selectend', this.onTriggerReleased.bind(this));
-    },
+    }
 
-    onTriggerPressed: function(event) {
-        if(event.inputSource.handedness == this.handedness) {
+    onTriggerPressed(event) {
+        if (event.inputSource.handedness == this.handedness) {
             this.heldGrip = this.overlappedGrip;
             this.object.getTranslationWorld(this.grippedPosition);
         }
-    },
-    onTriggerReleased: function(event) {
-        if(event.inputSource.handedness == this.handedness) {
+    }
+
+    onTriggerReleased(event) {
+        if (event.inputSource.handedness == this.handedness) {
             this.heldGrip = null;
         }
     }
-});
+}
